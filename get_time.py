5ce5671c2093b158ab0cfc85f288b8d35de7ba38
saper663_home <cheblin@yandex.ru>
2019-05-18 20:08:51 +0500

    add today()

--- get_time.py
+++ get_time.py
@@ -1,11 +1,15 @@
 import datetime
 
 
-def counts_remained_time_till_midnight_in_sec():
+def counts_remained_time_till_midnight_in_sec(): #выдаёт оставшееся время до 00:00
     now = datetime.datetime.now()
     time_string_of_ISO_format = now.time().isoformat(timespec='seconds')
     current_time_list_hour_min_sec = [int(i) for i in time_string_of_ISO_format.split(':')]
-    current_time_sec = 86400 - (current_time_list_hour_min_sec[0] * 3600 +
+    the_remained_time_sec = 86400 - (current_time_list_hour_min_sec[0] * 3600 +
     current_time_list_hour_min_sec[1] * 60 + current_time_list_hour_min_sec[2])
-    return current_time_sec
-    
+    return the_remained_time_sec
+
+def today(): # возвращает сегоднешнее число
+    current_date = datetime.date.today()
+    return (current_date.day)
+print(str(today()) + '\n')
