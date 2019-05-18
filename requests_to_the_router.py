263ba07d869119991ecc7cf6934ef2ee73e10ae0
saper663_home <cheblin@yandex.ru>
2019-05-18 20:21:35 +0500

    add requests_... new modul

---
+++ requests_to_the_router.py
@@ -0,0 +1,86 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+from consts import auth_form, auth_headers, auth_url_post, dhcp_list_url, dhcp_list_headers, continue_url
+from consts import deny_form, deny_headers, disable_form, disable_headers, url_deny_disable, continue_headers
+from consts import wireless_url, wireless_headers, mac_address
+from data_extraction import to_separate_mac_address, data_extraction_from_html
+
+# authorization -----------------------------------------
+
+def authorization():
+    r = requests.post(auth_url_post, headers=auth_headers, data=auth_form)
+    if r.status_code == 200:
+        print('access')
+    else:
+        print(r.status_code)
+# continue 'button'  come back at network filter menu  
+def get_continue():
+    r = requests.get(continue_url, headers=continue_headers)
+    if r.status_code == 200:
+        print('continue')
+    else:
+        print(r.status_code)
+
+
+# deny_post ------------------------------------------
+def mac_filter_on():
+    authorization()
+    r = requests.get(dhcp_list_url, headers=dhcp_list_headers) 
+    if r.status_code == 200:
+        dhcp_list = data_extraction_from_html(r.text, 'dhcp_list') # получение списка девайсов подкл. к роут.
+        deny_form['dhcp_list'] = dhcp_list   # изменнение списка в форме запроса
+    else:
+        return print(r.status_code)
+    if False: # проверка вкл. ли фильтер
+        print('already_Deny')
+    else:
+        r = requests.post(url_deny_disable, headers=deny_headers, data=deny_form)
+        if r.status_code == 200:
+            time.sleep(15)
+            print('DENY')
+            get_continue()
+        else:
+            print(r.status_code)
+    
+
+# disable_post --------------------------
+def mac_filter_off():
+    authorization()
+    r = requests.get(dhcp_list_url, headers=dhcp_list_headers)
+    if r.status_code == 200:
+        dhcp_list = data_extraction_from_html(r.text, 'dhcp_list')
+        deny_form['dhcp_list'] = dhcp_list
+    else:
+        return print(r.status_code)
+    time.sleep(5)
+    if True:
+        time.sleep(5)
+        r = requests.post(url_deny_disable, headers=disable_headers, data=disable_form)
+        if r.status_code == 200:
+            time.sleep(15)
+            print('DISABLE')
+            get_continue()
+        else:
+            print(r.status_code)
+    else:
+        print('already_disable')    
+# todo сделать проверку статуса мак_фильтра
+
+# check mac in wirelist
+def check_mac(mac_address):
+    authorization()
+    r = requests.get(wireless_url, headers=wireless_headers)
+    if r.status_code == 200:
+        if mac_address in to_separate_mac_address(mac_address):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+
+
+
+    
