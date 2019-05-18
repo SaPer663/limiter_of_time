e22f1c7d78b89afd60a8e449c54a42a54a4a7e51
saper663_home <cheblin@yandex.ru>
2019-05-18 20:05:47 +0500

    del isDeny and add to_separate_mac_address

--- data_extraction.py
+++ data_extraction.py
@@ -1,15 +1,13 @@
+import re
 from bs4 import BeautifulSoup
 
-def data_extraction_from_html(html_doc, id):
+def data_extraction_from_html(html_doc, id): 
     soup = BeautifulSoup(html_doc, features='lxml')
     try:
         return soup.find(attrs={'id': id})['value']
     except TypeError:
         return None
 
-def isDeny(html_doc):
-    if data_extraction_from_html(html_doc, 'mac_filter_00') != None:
-        return True
-    else:
-        return False
-        
+def to_separate_mac_address(string): # парсим список мак_адресов с веб-морды роутера
+    pattern = r'[0-9a-fA-F]{2}(?:[:-][0-9a-fA-F]{2}){5}' 
+    return re.findall(pattern, string)
