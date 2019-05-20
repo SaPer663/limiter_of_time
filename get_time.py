5187d92fd5e2e40b9930edeb833752d31c45df52
saper663_home <cheblin@yandex.ru>
2019-05-20 21:59:40 +0500

    rename counts_remained_time_till_midnight_in_sec on ..._till_9am_...., change today() and add current_time()

--- get_time.py
+++ get_time.py
@@ -1,16 +1,25 @@
 from datetime import datetime as dt
 
 
-def counts_remained_time_till_midnight_in_sec(): #выдаёт оставшееся время до 00:00
+
+def counts_remained_time_till_9am_in_sec(): #выдаёт оставшееся время до 09:00
     now = dt.now()
     time_string_of_ISO_format = now.time().isoformat(timespec='seconds')
     current_time_list_hour_min_sec = [int(i) for i in time_string_of_ISO_format.split(':')]
     the_remained_time_sec = 86400 - (current_time_list_hour_min_sec[0] * 3600 +
     current_time_list_hour_min_sec[1] * 60 + current_time_list_hour_min_sec[2])
-    return the_remained_time_sec
+    return the_remained_time_sec + 32400 # 32400 время от 00:00 до 9:00
 
 def today(): # возвращает сегоднешнее число
     current_date = dt.today()
     return current_date.day
+
+def current_time():
+    now = dt.now()
+    return now.hour
+
+
+
+    
 
 
